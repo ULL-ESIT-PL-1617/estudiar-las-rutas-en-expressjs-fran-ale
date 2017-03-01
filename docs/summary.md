26aef1228865a18dd1ddddc8fb77b3f49040e04a
franjpr <franpr90@gmail.com>
2017-03-01 15:30:43 +0000

    gitbook build

--- docs/summary.md
+++ docs/summary.md
@@ -1,6 +1,6 @@
 # Summary
 
-* [Introduccion](README.md)
+* [Introduction](README.md)
 * [Enrutamiento basico](enrutamiento.md)
 * [Middlewares](middlewares.md)
 * [Ejemplos](ejemplos.md)
---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp')
+var ghpages = require('gulp-gh-pages');
+var buildBranch = require('gulp-build-branch');
+var sshexec = require('ssh-exec');
+var exec = require('child_process').exec;
+var path = require('path').resolve() + '\\node_modules\\.bin\\';
+
+gulp.task('create-book', function(){
+  var cmd = 'gitbook build ./docs ./html';
+
+  exec(path + cmd, function(err, out, errout){
+    if(err) console.error('Error:' + err);
+    else    console.log('Executed ' + cmd);
+  });
+});
+
+gulp.task('deploy-ghpages', function () {
+  return gulp.src('./html/**/*').pipe(ghpages({"message" : 'GH-Pages deployed'}));
+});
