d6a9a7faf6e1165184cbf193f11b73b3aea6d9af
franjpr <franpr90@gmail.com>
2017-03-01 20:09:56 +0000

    gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp')
-var ghpages = require('gulp-gh-pages');
 var sshexec = require('ssh-exec');
 var exec = require('child_process').exec;
 var path = require('path').resolve() + '\\node_modules\\.bin\\';
@@ -8,7 +7,7 @@ var path = require('path').resolve() + '\\node_modules\\.bin\\';
 * Crea los .html al directorio publish a partir de los .md del dir ./docs
 */
 gulp.task('create-book', function(){
-  var cmd = 'gitbook build ./docs ./\.publish';
+  var cmd = 'gitbook build docs htmls';
   exec(path + cmd, function(err, out, errout){
     if(err) console.error('Error:' + err);
     else    console.log('Executed ' + cmd);
@@ -16,15 +15,22 @@ gulp.task('create-book', function(){
 });
 
 /*
-* Publica en la rama ghpages el contenio del directorio ./.publish
+* Publica en la rama ghpages el contenio del directorio ./html
 */
 gulp.task('deploy-ghpages', function () {
-  return gulp.src('./\.publish/**/*').pipe(ghpages({"message" : 'GH-Pages deployed'}));
+  exec('git checkout -b gh-pages && git checkout master', function(){
+    exec('git filter-branch --subdirectory-filter htmls gh-pages', function(){
+      exec('git push -f origin gh-pages', function(err, out, errout){
+        if(err) console.error('Error: ' + err)
+        else    console.log('commited changes');
+      });
+    });
+  });
 });
 
 /*
 * Crea una rama llamada gitbook, luego filtra el contenido de ./docs a
-* dicha rama y luego pushea la rama al master del remote de gitbook
+* dicha rama y luego pushea la rama al master del remote(gbook) de gitbook
 */
 gulp.task('deploy-gitbook', function() {
   exec('git checkout -b gitbook && git checkout master', function(){
