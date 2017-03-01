f91239f4bef7abb91912e90d8ad30970db9c10e6
franjpr <franpr90@gmail.com>
2017-03-01 21:13:54 +0000

    heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var app = express();
 
 app.listen(process.env.PORT || 8080);
-app.use(express.static(path.resolve('gh-pages')));
+app.use(express.static(path.resolve('htmls')));
 
 app.get('/', function(request, response){
   response.sendFile('htmls/index.html');
