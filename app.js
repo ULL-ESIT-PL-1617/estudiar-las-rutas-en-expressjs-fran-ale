4d34ba5ae1a1d802034f8cd507ebe3be4c098877
franjpr <franpr90@gmail.com>
2017-03-01 20:58:34 +0000

    despliegue heroku

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.listen(process.env.port || '8080');
+app.use(express.static(__dirname + "/htmls"));
+
+app.get('/', function(req, res){
+  res.send('index.html');
+});
+
+console.log('Running at localhost:'+ app.listen().address().port);
