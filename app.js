43b011f42d717c369366a1fd121c1f71a804d2a3
franjpr <franpr90@gmail.com>
2017-03-01 21:12:30 +0000

    heroku

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.listen(process.env.port || '8080');
-app.use(express.static(__dirname + "/htmls"));
+app.listen(process.env.PORT || 8080);
+app.use(express.static(path.resolve('gh-pages')));
 
-app.get('/', function(req, res){
-  res.sendFile('index.html');
+app.get('/', function(request, response){
+  response.sendFile('htmls/index.html');
 });
 
 console.log('Running at localhost:'+ app.listen().address().port);
